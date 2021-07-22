210fdad65a747d51aded9cadd9b9925eeae6a665
e <git@e.com>
2021-07-22 23:20:57 +0800

    订单以及库存服务模块的搭建

---
+++ orderserver/src/main/java/com/lvtuline/order/config/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package com.lvtuline.order.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                //  加了api注解Controller
+                .apis(RequestHandlerSelectors.basePackage("com.lvtuine.order.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                // 接口文档头
+                .title("lvtuline接口文档")
+                // 接口文档描述
+                .description("lvtuline接口文档")
+                // 服务器地址
+                .termsOfServiceUrl("https://www.lvtuline.com")
+                // 项目版本号
+                .version("1.0")
+                .build();
+    }
+
+}
---
+++ stockserver/src/main/java/com/lvtuline/stock/StockServerApplication.java
@@ -0,0 +1,13 @@
+package com.lvtuline.stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class StockServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApplication.class);
+    }
+}
